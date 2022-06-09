17814cc5247aad2bc2165edc1c68422484309e8c
Katelyn Chambers <kchambersofficial@yahoo.com>
2022-06-08 19:06:20 -0700

    bank user assignment

--- Python/bankuser.py
+++ Python/bankuser.py
@@ -3,11 +3,25 @@ from bankAccount import BankAccount
 
 class User:
 
-def __init__(self, name, email, account):
+def __init__(self, name, email, account, savings, checking):
      self.name = name 
      self.email = email 
      self.account = account 
+     self.savings = savings 
+     self.checking = checking
+
+
+     def make_deposit (self, amount, type):
+          self.accounts[type].deposit(amount) 
+         
+
+        def make_withdrawal (self, amount, type):
+            self.accounts[type].withdrawl(amount)
+
+    def display_user_balance(self):
+        self.accounts.display_account_info()
 
 savings = BankAccount(.1, 10000)
+checking = BankAccount(.1, 5000)
 
-Alden = User("Alden", "ac@gmail.com", savings)
+Alden = User("Alden", "ac@gmail.com", savings, checking)
